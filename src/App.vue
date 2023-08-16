fc514b3bcc442c09ed758878920a0a314e9987ba
Yamil Vernet <yamil.vernet@gmail.com>
2023-08-16 19:20:35 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>Hola</h1>
+        <div class="alert bg-success w-100">Este es un alert feliz</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
